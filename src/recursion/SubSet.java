e23978bda4636de396f0c0469db66413bbf2277c
amit <amitk.vishwa1633@gmail.com>
2026-03-29 20:56:56 +0530

    Made a new function in subset for handling duplicate element in arr

--- src/recursion/SubSet.java
+++ src/recursion/SubSet.java
@@ -1,14 +1,15 @@
 package recursion;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class SubSet {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int[] arr = { 1, 2, 3 };
-		List<List<Integer>> ans = subset(arr);
+		int[] arr = { 1, 2, 2 };
+		List<List<Integer>> ans = subsetDuplicate(arr);
 		for (List<Integer> list : ans) {
 			System.out.println(list);
 		}
@@ -31,4 +32,29 @@ public class SubSet {
 		return outer;
 	}
 
+	static List<List<Integer>> subsetDuplicate(int[] arr) {
+		Arrays.sort(arr);
+		List<List<Integer>> outer = new ArrayList<>();
+
+		outer.add(new ArrayList<>());
+		int start = 0;
+		int end = 0;
+
+		for (int i = 0; i < arr.length; i++) {
+			start = 0;
+			// If the current element is the same as previous one , s = e + 1;
+			if (i > 0 && arr[i] == arr[i - 1]) {
+				start = end + 1;
+			}
+			end = outer.size() - 1;
+			int n = outer.size();
+			for (int j = start; j < n; j++) {
+				List<Integer> internal = new ArrayList<Integer>(outer.get(j));
+				internal.add(arr[i]);
+				outer.add(internal);
+			}
+		}
+
+		return outer;
+	}
 }
