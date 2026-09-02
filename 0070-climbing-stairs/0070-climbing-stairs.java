5faf8a419733d665d0c3997eef9d2a8aecbcf360
Amitkumar Vishwakarma <89138634+Mit16@users.noreply.github.com>
2026-09-03 01:40:50 +0530

    Time: 0 ms (100%), Space: 41.9 MB (86.97%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -0,0 +1,18 @@
+class Solution {
+    
+    public int climbStairs(int n) {
+       int[] memo= new int[n+1];
+        return recur(n,memo);
+    }
+
+    private int recur(int n,int[] memo){
+        if(n==1) return 1;
+        if(n==2) return  2;
+
+        if(memo[n]!=0) return memo[n];
+
+        memo[n]=recur(n-1,memo) + recur(n-2,memo);
+
+        return memo[n];
+    }
+}
