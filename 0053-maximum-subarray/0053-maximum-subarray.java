b2c7113a78acb1f72ecf0353e0ad8fbe5c2baae5
Amitkumar Vishwakarma <89138634+Mit16@users.noreply.github.com>
2026-09-02 23:51:48 +0530

    Time: 1 ms (99.94%), Space: 77.3 MB (32.54%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -1,36 +1,14 @@
 class Solution {
-
-    private int[] memo;
-
     public int maxSubArray(int[] nums) {
+        int maxSum = nums[0];
+        int currentSum = nums[0];
 
-        memo = new int[nums.length];
-        Arrays.fill(memo, Integer.MIN_VALUE);
-
-        int answer = Integer.MIN_VALUE;
-
-        for (int i = 0; i < nums.length; i++) {
-            answer = Math.max(answer, recurse(nums, i));
+        for (int i = 1; i < nums.length; i++) {
+            currentSum = Math.max(nums[i], currentSum + nums[i]);
+            maxSum = Math.max(currentSum, maxSum);
         }
 
-        return answer;
-    }
+        return maxSum;
 
-    private int recurse(int[] nums, int idx) {
-
-        if (idx == 0) {
-            return nums[0];
-        }
-
-        if (memo[idx] != Integer.MIN_VALUE) {
-            return memo[idx];
-        }
-
-        int current = Math.max(
-            nums[idx],
-            nums[idx] + recurse(nums, idx - 1)
-        );
-
-        return memo[idx] = current;
     }
 }
