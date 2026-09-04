8a482b7d49352fc4b123a925464e03c7d68147d1
Amitkumar Vishwakarma <89138634+Mit16@users.noreply.github.com>
2026-09-05 01:18:52 +0530

    Time: 6 ms (16.98%), Space: 48.6 MB (50.07%) - LeetHub

---
+++ 0338-counting-bits/0338-counting-bits.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int[] countBits(int n) {
+        int[] result = new int[n + 1];
+        for (int i = 0; i <= n; i++) {
+            result[i] = bitsCounter(i);
+        }
+
+        return result;
+
+    }
+
+    private int bitsCounter(int n) {
+        int result = 0;
+        while (n > 0) {
+            if ((n & 1) == 1)
+                result++;
+            n = n >> 1;
+        }
+
+        return result;
+    }
+}
