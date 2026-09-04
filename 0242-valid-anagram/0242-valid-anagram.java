dccffc2fb574dbcbc92657943d009f27925627d7
Amitkumar Vishwakarma <89138634+Mit16@users.noreply.github.com>
2026-09-05 00:23:33 +0530

    Time: 4 ms (83.57%), Space: 44.9 MB (52.97%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -10,8 +10,8 @@ class Solution {
             freq[c - 'a']--;
         }
 
-        for (int i = 0; i < 26; i++) {
-            if (freq[i] != 0)
+        for (int i :freq) {
+            if (i != 0)
                 return false;
         }
 
