4d04ea382ec93c72ca4a4fb03bf946dc93c41f7c
Amitkumar Vishwakarma <89138634+Mit16@users.noreply.github.com>
2026-09-21 14:54:01 +0530

    Time: 3 ms (36.94%), Space: 48.9 MB (26.53%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -1,17 +1,20 @@
 class Solution {
     public List<String> letterCombinations(String digits) {
-        String[] comb = new String[]{"abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+        String[] comb = new String[] { "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz" };
 
         List<String> result = new ArrayList<>();
+        if (digits.length() == 0) {
+            return result;
+        }
         result.add("");
 
-        for(char c: digits.toCharArray()){
+        for (char c : digits.toCharArray()) {
             List<String> temp = new ArrayList<>();
             String tString = comb[c - '2'];
 
-            for(String i: result){
-                for(char t : tString.toCharArray()){
-                    temp.add(i+t);
+            for (String i : result) {
+                for (char t : tString.toCharArray()) {
+                    temp.add(i + t);
                 }
             }
 
