366b039a970d2d5576a1bd2eed4535e447abd056
Amitkumar Vishwakarma <89138634+Mit16@users.noreply.github.com>
2026-09-21 14:48:50 +0530

    Time: 3 ms (36.94%), Space: 43.6 MB (44.64%) - LeetHub

---
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -0,0 +1,23 @@
+class Solution {
+    public List<String> letterCombinations(String digits) {
+        String[] comb = new String[]{"abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+
+        List<String> result = new ArrayList<>();
+        result.add("");
+
+        for(char c: digits.toCharArray()){
+            List<String> temp = new ArrayList<>();
+            String tString = comb[c - '2'];
+
+            for(String i: result){
+                for(char t : tString.toCharArray()){
+                    temp.add(i+t);
+                }
+            }
+
+            result = temp;
+        }
+
+        return result;
+    }
+}
